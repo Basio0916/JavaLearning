c1ad55d4230c5def3122b110c28903c372837a2e
arsoncase <arsoncase@che>
2022-12-28 01:18:16 +0000

    175p

--- PizzaStore/src/main/java/com/mishibashi/NYPizzaStore.java
+++ PizzaStore/src/main/java/com/mishibashi/NYPizzaStore.java
@@ -1,7 +1,28 @@
 package com.mishibashi;
 
 public class NYPizzaStore extends PizzaStore{
-    Pizza createPizza(String type){
-        return new NYStyleCheesePizza();
+    protected Pizza createPizza(String type){
+        Pizza pizza = null;
+        PizzaIngredientFactory ingredientFactory = new NYPizzaIngredientFactory();
+
+        switch(type){
+            case "チーズ":
+            pizza = new CheesePizza(ingredientFactory);
+            pizza.setName("ニューヨークスタイルチーズピザ");
+            break;
+            case "野菜":
+            pizza = new VeggiePizza(ingredientFactory);
+            pizza.setName("ニューヨークスタイル野菜ピザ");
+            break;
+            case "アサリ":
+            pizza = new ClamPizza(ingredientFactory);
+            pizza.setName("ニューヨークスタイルアサリピザ");
+            break;
+            case "ペパロニ":
+            pizza = new PepperoniPizza(ingredientFactory);
+            pizza.setName("ニューヨークスタイルペパロニピザ");
+            break;
+        }
+        return pizza;
     }
 }
