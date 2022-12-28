d0e0f7384dc7d67178da28aeb85b0550d4e7727a
arsoncase <arsoncase@che>
2022-12-28 05:12:41 +0000

    219p

--- Command/src/main/java/com/mishibashi/Command.java
+++ Command/src/main/java/com/mishibashi/Command.java
@@ -2,4 +2,5 @@ package com.mishibashi;
 
 public interface Command {
     public void execute();
+    public void undo();
 }
--- Command/src/main/java/com/mishibashi/NoCommand.java
+++ Command/src/main/java/com/mishibashi/NoCommand.java
@@ -2,4 +2,5 @@ package com.mishibashi;
 
 public class NoCommand implements Command{
     public void execute(){}
+    public void undo(){}
 }
