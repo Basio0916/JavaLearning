59e8c5f24ade2566a9ba51e4c283291c988f8690
arsoncase <arsoncase@che>
2023-01-05 06:10:32 +0000

    323p

---
+++ Iterator/src/main/java/com/mishibashi/DinerMenu.java
@@ -0,0 +1,31 @@
+package com.mishibashi;
+
+public class DinerMenu {
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public DinerMenu(){
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("ベジタリアンBLT", "レタス、トマト、（フェイク）ベーコンをはさんだ全粒小麦パンサンドイッチ", true, 2.99);
+        addItem("BLT", "レタス、トマト、ベーコンをはさんだ全粒小麦パンサンドイッチ", false, 2.99);
+        addItem("本日のスープ", "ポテトサラダを添えた本日のスープ", false, 3.29);
+        addItem("Hotdog", "ザワークラウト、レリッシュ、玉ねぎ、チーズをはさんだホットドッグ", false, 3.05);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if(numberOfItems >= MAX_ITEMS){
+            System.err.println("すみません、メニューはいっぱいです！メニューに項目を追加できません。");
+        }
+        else{
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems++;
+        }
+    }
+
+    public MenuItem[] getMenuItems(){
+        return menuItems;
+    }
+}
---
+++ Iterator/src/main/java/com/mishibashi/PancakeHouseMenu.java
@@ -0,0 +1,26 @@
+package com.mishibashi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeHouseMenu {
+    List<MenuItem> menuItems;
+
+    public PancakeHouseMenu(){
+        menuItems = new ArrayList<MenuItem>();
+
+        addItem("K&Bのパンケーキ朝食", "スクランブルエッグとトースト付きパンケーキ", true, 2.99);
+        addItem("いつものパンケーキ朝食", "卵焼きとソーセージ付きパンケーキ", false, 2.99);
+        addItem("ブルーベリーパンケーキ", "新鮮なブルーベリーを使ったパンケーキ", true, 3.49);
+        addItem("ワッフル", "ブルーベリーか苺の好きな方を乗せたワッフル", true, 3.59);
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price){
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+
+    public List<MenuItem> getMenuItems(){
+        return menuItems;
+    }
+}
