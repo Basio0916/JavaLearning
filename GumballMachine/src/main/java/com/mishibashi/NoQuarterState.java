83022aa164c6847f9e111173bae2ff449ea8d012
arsoncase <arsoncase@che>
2023-01-10 02:44:43 +0000

    412p

---
+++ GumballMachine/src/main/java/com/mishibashi/NoQuarterState.java
@@ -0,0 +1,31 @@
+package com.mishibashi;
+
+public class NoQuarterState implements State{
+    GumballMachine gumballMachine;
+
+    public NoQuarterState(GumballMachine gumballMachine){
+        this.gumballMachine = gumballMachine;
+    }
+
+    @Override
+    public void insertQuarter() {
+        System.out.println("25セントを投入しました");
+        gumballMachine.setState(gumballMachine.getHasQuarterState());
+    }
+
+    @Override
+    public void ejectQuarter() {
+        System.out.println("25セントを投入していません");
+    }
+
+    @Override
+    public void turnCrank() {
+        System.out.println("ハンドルを回しましたが、25セントを投入していません");
+    }
+
+    @Override
+    public void dispense() {
+        System.out.println("まずお金を払う必要があります");
+    }
+    
+}
