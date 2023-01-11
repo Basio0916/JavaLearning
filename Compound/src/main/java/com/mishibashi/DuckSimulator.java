ae34dd984d22133e97999fbbf219397ccd48aa9c
basio0916 <arsoncase@gmail.com>
2023-01-11 15:00:43 +0900

    509p

--- Compound/src/main/java/com/mishibashi/DuckSimulator.java
+++ Compound/src/main/java/com/mishibashi/DuckSimulator.java
@@ -8,18 +8,36 @@ public class DuckSimulator {
     }
 
     void simulate(AbstractDuckFactory duckFactory){
-        Quackable mallardDuck = duckFactory.createMallardDuck();
         Quackable redheadDuck = duckFactory.createRedheadDuck();
         Quackable duckCall = duckFactory.createDuckCall();
         Quackable rubberDuck = duckFactory.createRubberDuck();
         Quackable gooseDuck = new GooseAdapter(new Goose());
 
-        System.out.println("\nカモシミュレーター");
-        simulate(mallardDuck);
-        simulate(redheadDuck);
-        simulate(duckCall);
-        simulate(rubberDuck);
-        simulate(gooseDuck);
+        System.out.println("\nカモシミュレーター：コンポジット（群れ）付き");
+        Flock flockOfDucks = new Flock();
+        flockOfDucks.add(redheadDuck);
+        flockOfDucks.add(duckCall);
+        flockOfDucks.add(rubberDuck);
+        flockOfDucks.add(gooseDuck);
+
+        Flock flockOfMallards = new Flock();
+        Quackable mallardOne = duckFactory.createMallardDuck();
+        Quackable mallardTwo = duckFactory.createMallardDuck();
+        Quackable mallardThree = duckFactory.createMallardDuck();
+        Quackable mallardFour = duckFactory.createMallardDuck();
+
+        flockOfMallards.add(mallardOne);
+        flockOfMallards.add(mallardTwo);
+        flockOfMallards.add(mallardThree);
+        flockOfMallards.add(mallardFour);
+
+        flockOfDucks.add(flockOfMallards);
+
+        System.out.println("\nカモシミュレーター：群れ全体のシミュレーション");
+        simulate(flockOfDucks);
+
+        System.out.println("\nカモシミュレーター：マガモの群れのシミュレーション");
+        simulate(flockOfMallards);
 
         System.out.println("カモが鳴いた回数 : " + QuackCounter.getQuacks() + "回");
     }
